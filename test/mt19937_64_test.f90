21c1aa33519a8820ac343cb08550cd5cde28d0a1
Jacob Williams <jacobwilliams@users.noreply.github.com>
2022-05-01 22:36:02 -0500

    test all the routines

--- test/mt19937_64_test.f90
+++ test/mt19937_64_test.f90
@@ -5,7 +5,7 @@
 program mt19937_64_test
 
     use mt19937_64
-    use iso_fortran_env, only: output_unit, wp => real64
+    use iso_fortran_env, only: output_unit, wp => real64, i4 => int32, i8 => int64
 
     implicit none
 
@@ -19,6 +19,24 @@ program mt19937_64_test
         r = random%genrand64_real1()
         write(output_unit, '(E30.16)') r
     end do
+
+    call random%initialize(1776_i8)
+
+    do i = 1, 10
+        r = random%genrand64_real1()
+        write(output_unit, '(E30.16)') r
+    end do
+
+    call random%initialize([1_i8,22_i8,333_i8])
+
+    r = random%genrand64_real1()
+    write(output_unit, '(E30.16)') r
+
+    r = random%genrand64_real2()
+    write(output_unit, '(E30.16)') r
+
+    r = random%genrand64_real3()
+    write(output_unit, '(E30.16)') r
 
 end program mt19937_64_test
 !*****************************************************************************************
